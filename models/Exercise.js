7f92178a9fd2f142ff9e528a1fff02aaa03f3dba
Justin Hoch <justinhoch22@gmail.com>
2021-08-15 14:59:57 -0500

    Edited exercise model typo

--- models/Exercise.js
+++ models/Exercise.js
@@ -15,6 +15,6 @@ const ExerciseSchema = new Schema({
     }
 });
 
-const Exercise = mongoose.model("Book", ExerciseSchema);
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
 module.exports = Exercise;
