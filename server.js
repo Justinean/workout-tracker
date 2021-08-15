f70fab0c549009f5ff7aef834f1f1568ed2da1ca
Justin Hoch <justinhoch22@gmail.com>
2021-08-15 15:20:33 -0500

    Added index.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const routes = require('./controllers');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +11,7 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(routes);
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
