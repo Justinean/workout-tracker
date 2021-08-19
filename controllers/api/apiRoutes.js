60b5e6c12f96e96307f61291b9831743fc5cd6d8
Justin Hoch <justinhoch22@gmail.com>
2021-08-19 01:09:18 -0500

    Added /workouts/:id route and edited some of the other routes

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -5,18 +5,9 @@ const db = require('../../models');
 router.get('/workouts', async (req, res) => {
     let data = await db.Workout.find();
     let newdata = [];
-    for (let i = 0; i < data.length; i++) {
-        newdata.push({
-            _id: data[i]._id,
-            day: data[i].day,
-            exercises: data[i].exercises,
-            totalDuration: data[i].exercises[0].duration
-        })
-        for (let j = 0; j < data.length; j++) {
-            if (i !== j && data[i].day.toLocaleString().split(",")[0].split("/")[1] === data[j].day.toLocaleString().split(",")[0].split("/")[1]) {
-                newdata[i].totalDuration += data[j].exercises[0].duration;
-            }
-        }
+    newdata.push({_id: data[data.length - 1]._id, day: data[data.length - 1].day, exercises: data[data.length - 1].exercises, totalDuration: 0})
+    for (let i = 0; i < newdata[0].exercises.length; i++) {
+        newdata[0].totalDuration += newdata[0].exercises[i].duration;
     }
     res.json(newdata)
 })
@@ -24,17 +15,10 @@ router.get('/workouts', async (req, res) => {
 router.get('/workouts/range', async (req, res) => {
     let data = await db.Workout.find();
     let newdata = [];
-    for (let i = 0; i < data.length; i++) {
-        newdata.push({
-            _id: data[i]._id,
-            day: data[i].day,
-            exercises: data[i].exercises,
-            totalDuration: data[i].exercises[0].duration
-        })
-        for (let j = 0; j < data.length; j++) {
-            if (i !== j && data[i].day.toLocaleString().split(",")[0].split("/")[1] === data[j].day.toLocaleString().split(",")[0].split("/")[1]) {
-                newdata[i].totalDuration += data[j].exercises[0].duration;
-            }
+    for (let i = data.length - 7; i < data.length; i++) {
+        newdata.push({_id: data[i]._id, day: data[i].day, exercises: data[i].exercises, totalDuration: 0})
+        for (let j = 0; j < newdata[i - (data.length - 7)].exercises.length; j++) {
+            newdata[i - (data.length - 7)].totalDuration += newdata[i - (data.length - 7)].exercises[j].duration;
         }
     }
     res.json(newdata)
@@ -46,6 +30,17 @@ router.post('/workouts', async (req, res) => {
         exercises: []
     }
     await db.Workout.collection.insert(data)
+    res.json(data)
+})
+
+router.put('/workouts/:id', async (req, res) => {
+    let data = await db.Workout.find({ _id: req.params.id });
+    let exercises = data[0].exercises
+    let object = req.body;
+    exercises.push(object)
+    await db.Workout.updateOne({ _id: req.params.id }, { $set: { exercises } })
+    data = await db.Workout.find({ _id: req.params.id });
+    res.json(data);
 })
 
 module.exports = router;
